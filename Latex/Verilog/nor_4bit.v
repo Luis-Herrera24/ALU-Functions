158bb3227b3165804d5c7dfe07a229d967842e2f
Luis Herrera <130265201+Luis-Herrera24@users.noreply.github.com>
2024-08-21 13:52:54 -0500

    Delete Latex/Verilog directory

--- Latex/Verilog/nor_4bit.v
+++
@@ -1,9 +0,0 @@
-module nor_4bit(
-    input [3:0] A,
-    input [3:0] B,
-    output [3:0] Y
-);
-
-assign Y = ~(A | B); // 4-bit NOR operation
-
-endmodule
--- Latex/Verilog/xnor_4bit.v
+++
@@ -1,9 +0,0 @@
-module xnor_4bit (
-    input [3:0] A,
-    input [3:0] B,
-    output [3:0] Y
-);
-
-assign Y = ~(A ^ B);
-
-endmodule
