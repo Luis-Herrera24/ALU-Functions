0511dc722b8c674439db20924243ff1fee4148b9
Luis Herrera <130265201+Luis-Herrera24@users.noreply.github.com>
2024-08-21 13:44:26 -0500

    Delete Leftovers-ProjectStep# 3 /Latex directory

--- Leftovers-ProjectStep# 3 /Latex/Verilog/nand_4bit.v
+++
@@ -1,9 +0,0 @@
-module nand_4bit(
-    input [3:0] A,
-    input [3:0] B,
-    output [3:0] Y
-);
-
-assign Y = ~(A & B); // 4-bit NAND operation
-
-endmodule
--- Leftovers-ProjectStep# 3 /Latex/Verilog/not_4bit.v
+++
@@ -1,8 +0,0 @@
-module not_4bit (
-    input [3:0] A,
-    output [3:0] Y
-);
-
-assign Y = ~A;
-
-endmodule
